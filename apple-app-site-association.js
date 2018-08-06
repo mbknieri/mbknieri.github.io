df7467fdfcd96300cc279cdf8141b132eb4e1095
Marissa Moore <Marissa.Moore2@panerabread.com>
2018-08-06 13:42:43 -0400

    Adding association file to repo

---
+++ apple-app-site-association.js
@@ -0,0 +1,28 @@
+{
+  "webcredentials": {
+    "apps": [
+      "4C6B2867MN.com.panerabread.mobile.inhouse.qa"
+    ]
+  },
+  "applinks": {
+    "apps": [],
+    "details": [
+      {
+        "appID": "4C6B2867MN.com.panerabread.mobile.inhouse.qa",
+        "paths": [
+          "/myCustomPattern/",
+          "/track*",
+          "/",
+          "/#!/",
+          "*/orderProcess/",
+          "*/cafeLocations/",
+          "*/menu/category/*",
+          "*/account/my_past_orders",
+          "*/account/my_favorites",
+          "*/account/my_rewards",
+          "*/account/my_account_info"
+        ]
+      }
+    ]
+  }
+}
